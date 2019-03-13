489696370f4dbf8d531f71a50c6ed953e0344001
AllanC56 <allan.comisso56@gmail.com>
2019-03-14 00:27:32 +0100

    Clean code

--- js/data.js
+++ js/data.js
@@ -7,7 +7,6 @@ function requestUserInfos() {
                 document.getElementById("name").innerHTML = response.name;
                 document.getElementById("login").innerHTML = response.login;
                 document.getElementById("bio").innerHTML = response.bio;
-
                 document.getElementById("location").innerHTML += response.location;
                 document.getElementById("public_repos").innerHTML += response.public_repos + " Public Repos";
                 if (response.hireable == true) {
--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     $('.preloader-background').delay(1000).fadeOut('slow');
     $('.preloader-wrapper').delay(1000).fadeOut();
-
 });
